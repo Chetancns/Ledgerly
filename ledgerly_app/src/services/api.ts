24e1b6425b2954bb1865bad49b38bbf7efa5d10f
Chetan.N.S <chethanns498@gmail.com>
2025-10-19 15:31:12 -0400

    updated next.config.ts
    Skipping validation of types
    Skipping linting

--- ledgerly_app/src/services/api.ts
+++ ledgerly_app/src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+//console.log("API Base URL:", process.env.NEXT_PUBLIC_API_BASE_URL);
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://192.168.1.50:3001", // use env fallback to previous IP
   withCredentials: true,
