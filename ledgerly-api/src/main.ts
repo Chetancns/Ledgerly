18863a6c0441c748cd96bc45bceb94fc01457579
Chetan.N.S <chethanns498@gmail.com>
2025-10-19 15:38:08 -0400

    update CORS URL

--- ledgerly-api/src/main.ts
+++ ledgerly-api/src/main.ts
@@ -6,7 +6,7 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
-  app.enableCors({ origin: ['http://localhost:3000', 'http://192.168.1.50:3000'], credentials: true });
+  app.enableCors({ origin: ['http://localhost:3000', 'http://192.168.1.50:3000','https://ledgerly-eight.vercel.app'], credentials: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.PORT ?? 3000, () => {
   console.log(`App listening on port ${process.env.PORT ?? 3000}`);
